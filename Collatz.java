50ae1cecab431eb8c094a18496afb76b47c35424
OrDavidovitz <or.davidovitz@post.runi.ac.il>
2024-11-16 11:53:37 +0200

    Hw2

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,63 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
-}
+	    int N = Integer.parseInt(args[0]);
+		int F ;
+		String str = args[1];	
+		if (str.equals("V"))
+		{
+				for (int i = 1; i <= N; i++)
+				{
+					int counter = 0;
+					if (i%2!=0)
+					{
+						System.out.print(i + " ");
+						counter+=1;
+						F=i*3+1;
+						while (F!=1)
+						{
+							if (F%2==0)
+							{
+								System.out.print(F + " ");
+								counter+=1;
+								F/=2;
+							}
+							else
+							{
+								System.out.print(F + " ");
+								counter+=1;
+								F=F*3+1;
+							}
+						}
+						System.err.print(F + " ");
+						counter++;
+					}	
+					else
+					{
+						System.out.print(i + " ");
+						counter++;
+						F=i;
+						while (F!=1)
+						{ 
+							if (F%2==0) 
+							{
+							F/=2;
+							System.out.print(F + " ");
+							counter++;
+							}
+							else
+							{
+								F=F*3+1;
+								System.out.print(F + " ");
+								counter++;
+							}
+						}
+
+					}
+					System.out.print(" (" + counter+")");
+					System.out.println();
+				}
+		}
+				System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
+			}
+		}
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,12 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutestoAdd = Integer.parseInt(args[1]);
+        int totalMinutes = hours * 60 + minutes;
+        int sum = totalMinutes + minutestoAdd;
+        int newHours = (sum / 60) % 24; 
+         int newMinutes = sum % 60;
+        System.out.println(newHours + ":" + newMinutes);
     }
 }
